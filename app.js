8d629474d511e43a2cd22f40880b8c5c19415579
Dora Belme <contact.dorabelme@gmail.com>
2020-02-24 20:10:52 -0500

    chat functionality JS

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
 const messageTypes = { LEFT: 'left', RIGHT: 'right', LOGIN: 'login' };
 
 // Chat Stuff
-const chatWindow = document.getElementById('chatWindow');
+const chatWindow = document.getElementById('chat');
 const messagesList = document.getElementById('messagesList');
 const messageInput = document.getElementById('messageInput');
 const sendBtn = document.getElementById('sendBtn');
 
 // Login Stuff
+let username = '';
 const usernameInput = document.getElementById('usernameInput');
 const loginBtn = document.getElementById('loginBtn');
-const loginWindow = document.getElementById('loginWindow');
+const loginWindow = document.getElementById('login');
 
-const messages = [
-    {
-    author: 'Dora Belme',
-    date: '2/24/2020',
-    content: 'Cool message',
-    type: messageTypes.RIGHT
-    },
-    {
-        author: 'Dora Belme',
-        date: '2/24/2020',
-        content: 'Cool message',
-        type: messageTypes.LEFT
-    },
-    {
-        author: 'Dora Belme',
-        date: '2/24/2020',
-        type: messageTypes.LOGIN
-    }]; // {author, date, content, type}
-
+const messages = []; // {author, date, content, type}
 
 // take in message object, and return corresponding message HTML
 const createMessageHTML = message => {
@@ -38,18 +21,74 @@ const createMessageHTML = message => {
     }
 
     return `
-        <div class="message ${message.type === messageTypes.LEFT ? 'message-left' : 'message-right'}">
+        <div class="message ${
+            message.type === messageTypes.LEFT
+                ? 'message-left'
+                : 'message-right'
+        }">
             <div id="message-details" class="flex">
-                <p class="message-author">${message.author === messageTypes.RIGHT ? '': message.author}</p>
+                <p class="message-author">${
+                    message.type === messageTypes.RIGHT ? '' : message.author
+                }</p>
                 <p class="message-date">${message.date}</p>
             </div>
             <p class="message-content">${message.content}</p>
-        </div>`
-}
+        </div>`;
+};
 
 const displayMessages = () => {
-    const messagesHTML = messages.map(message => createMessageHTML(message)).join('');
+    const messagesHTML = messages
+        .map(message => createMessageHTML(message))
+        .join('');
     messagesList.innerHTML = messagesHTML;
-}
+};
 
 displayMessages();
+
+// sendBtn callback
+sendBtn.addEventListener('click', e => {
+    // preventDefault of a fomr
+    e.preventDefault();
+
+    // set the username and create logged in message
+    if (!messageInput.value) {
+        return console.log('must supply a username');
+    }
+
+    const message = {
+        author: username,
+        date: new Date(),
+        content: messageInput.value,
+        type: messageTypes.RIGHT,
+    };
+
+    messages.push(message);
+    displayMessages();
+
+    messageInput.value = '';
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+});
+
+// loginBtn callback
+loginBtn.addEventListener('click', e => {
+    // preventDefault of a fomr
+    e.preventDefault();
+
+    // set the username and create logged in message
+    if (!usernameInput.value) {
+        return console.log('must supply a username');
+    }
+    username = usernameInput.value;
+
+    messages.push({
+        author: username,
+        type: messageTypes.LOGIN,
+    });
+
+    // hide login and show chat window
+    loginWindow.classList.add('hidden');
+    chatWindow.classList.remove('hidden');
+
+    // display those messages
+    displayMessages();
+});
